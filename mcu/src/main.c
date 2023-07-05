f894b249eee4890f6112411f6aee53af21d62b0e
Emma Wadsworth <emma.a.wadsworth@gmail.com>
2023-07-05 14:19:22 -0700

    output pulled low for all unused pins

--- mcu/src/main.c
+++ mcu/src/main.c
@@ -22,6 +22,30 @@ struct dac_t dac3 =
     .port_bit = BIT5,
 };
 
+void init_unused(void)
+{
+    //this just goes through every pin that is not
+    //directly necessary, and puts it to output, pulled low
+
+    P1DIR |= BIT5 | BIT4 | BIT3 | BIT2 | BIT1;
+    P1OUT &= ~(BIT5 | BIT4 | BIT3 | BIT2 | BIT1);
+
+    P2DIR |= BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P2OUT &= ~(BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+
+    P3DIR |= BIT7 | BIT6 | BIT4 | BIT3 | BIT2 |BIT0;
+    P3OUT &= ~(BIT7 | BIT6 | BIT4 | BIT3 | BIT2 | BIT0);
+
+    P4DIR |= BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P4OUT &= ~(BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+
+    P5DIR |= BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P5OUT &= ~(BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+
+    P6DIR |= BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0;
+    P6OUT &= ~(BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+}
+
 
 void init(void)
 {
@@ -38,6 +62,8 @@ void init(void)
     init_dac(dac3);
 
     init_uart();
+
+    init_unused();
 }
 
 
