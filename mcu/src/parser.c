91b8b4433406a4d2e8c9afa3257dcbe339c75249
Trevor Vannoy <trevorvannoy@montana.edu>
2023-09-18 14:39:44 -0600

    clean up parsing functions
    - split long conditionals into variables
    - use logical operators (&&,||) instead of bitwise operators (*,|)

--- mcu/src/parser.c
+++ mcu/src/parser.c
@@ -15,7 +15,8 @@ const char const pmt_type_str[] =
 bool parse_command(const char *input, pmt_data_t *parsed)
 {
     // validate command format
-    if(!validate_format(input))
+    bool format_is_valid = validate_format(input);
+    if(!format_is_valid)
     {
         return false;
     }
@@ -35,7 +36,11 @@ bool parse_command(const char *input, pmt_data_t *parsed)
     // if all is well, store the PMT type and voltage in the parsed structure and return true,
     // otherwise, return false to tell the caller that the command isn't valid
     //uint16_t bound = pmts[parsed->pmt].control_voltage_upper_bound;
-    if(!validate_voltage_bounds(voltage,pmts[parsed->pmt].control_voltage_upper_bound,pmts[parsed->pmt].control_voltage_lower_bound))
+    bool voltage_bounds_are_valid = validate_voltage_bounds(
+            voltage,pmts[parsed->pmt].control_voltage_upper_bound,
+            pmts[parsed->pmt].control_voltage_lower_bound
+            );
+    if(!voltage_bounds_are_valid)
     {
         return false;
     }
@@ -46,19 +51,25 @@ bool parse_command(const char *input, pmt_data_t *parsed)
 //checks that length is 5, first char is SIGNAL or TRIGGER char, and remaining chars are digits
 bool validate_format(const char *input)
 {
-    if(strlen(input) == 6 &
-       (input[0] == pmt_type_str[SIGNAL] | input[0] == pmt_type_str[TRIGGER]) &
-       (isdigit(input[1]) & isdigit(input[2]) & isdigit(input[3]) & isdigit(input[4])))
+    bool is_correct_length = strlen(input) == 6;
+    bool is_valid_pmt_type = input[0] == pmt_type_str[SIGNAL] || input[0] == pmt_type_str[TRIGGER];
+    bool has_4_numbers = isdigit(input[1]) && isdigit(input[2]) && isdigit(input[3]) && isdigit(input[4]);
+
+    if(is_correct_length && is_valid_pmt_type && has_4_numbers)
     {
         return true;
     }
-    return false;
+    else
+    {
+        return false;
+    }
+
 }
 
 // might put this into parse_command if we stop caring about tests
 bool validate_voltage_bounds(const uint16_t voltage, const uint16_t upper_bound, const uint16_t lower_bound)
 {
-    if(voltage > upper_bound | voltage < lower_bound)
+    if(voltage > upper_bound || voltage < lower_bound)
     {
         return false;
     }
